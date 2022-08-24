fc493797846ff870006080269dccc47447a7c10c
Błażej Domagała <petitoff@outlook.com>
2022-08-24 19:12:24 +0200

    update

---
+++ src/FriendOrganiser.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FriendOrganiser.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FriendOrganiser.DataAccess
+{
+    public class FriendOrganizerDbContext : DbContext
+    {
+        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Friend>()
+                .Property(f=>f.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+        }
+    }
+}
